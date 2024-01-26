05df44efa3fb5e6b91c7074ef6f964172d5062c1
Hüsrev <Mhozbalik@gmail.com>
2024-01-27 02:28:30 +0300

    Game Manager Initialize

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void NextScene()
+    {
+        // End Scene Animation
+    }
+    public void StartScene()
+    {
+        // Start Scene Animation
+    }
+
+    public void PauseScreen() { }
+
+    public void Restart() { }
+}
